866c62cb7da0cff5c2f88e595b9da89e6f55a955
cdh616x <91150236+cdh616x@users.noreply.github.com>
2022-02-10 00:05:24 -0500

    able to change company name, file name, and position, albeit rather primatively

--- main.py
+++ main.py
@@ -1,13 +1,27 @@
-list = ["Lucky", "Collin", "Sixes"]
+def create_new_coverletter(params):
+    list = [f"{params}"]
+    with open("company.txt") as letter_file:
+        letter_contents = letter_file.read()
+        company = input("What company are you sending this application to?")
+        for i in list:
+            x = letter_contents.replace("[company_name]", company)
+            with open(f"./output/{i}_cover_letter.txt", mode="w") as comp_letter:
+                comp_letter.write(x)
 
-print(list)
 
-PLACEHOLDER = "[name]"
+def replace_position(params, file):
+    list = [f"{params}"]
+    with open(f"{file}") as letter_file:
+        letter_contents = letter_file.read()
+        x = letter_contents.replace("[open_position]", params)
+        with open(f"{file}", mode="w") as pos_letter:
+            pos_letter.write(x)
 
-with open("company.txt") as letter_file:
-    letter_contents = letter_file.read()
-    for i in list:
-        x = letter_contents.replace("[name]", i)
-        print(f"{x}\n\n")
-        with open(f"./output/{i}.txt", mode="a") as comp_letter:
-            comp_letter.write(x)
+
+subject = str(input("What would you like to prefix the file's name with?"))
+create_new_coverletter(subject)
+
+position_title = str(input("What is the name of the position you are applying for?"))
+file_name = str(input("What is the name of the file?"))
+replace_position(position_title, "./output/" + file_name)
+
